a2249dbba731cee8fc3b7f598877cd3067e7b35a
TSpruill <tivona.spruill@yahoo.com>
2017-08-02 16:05:34 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+function information() {
+
+let body = document.getElementByClassName("body")[0];
+
+let container = document.createElement("container")
+let div = document.createElement("div");
+let h = document.createElement("h1");
+let t = document.createTextNode("Internal Company Directory");
+
+body.appendChild(container);
+conatiner.appendChild(div);
+div.appendChild(h1);
+h1.appendChild(Internal Companay Directory);
+
+}
+
+information();
+
+for (let i = 0; i < customers.results.length; i++){
+
+}
+console.log(customers.results[0].name.first)
